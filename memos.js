18dda35ef24acabd451c88af9196a7f414c93ec1
EC2 Default User <ec2-user@ip-172-31-45-76.us-east-2.compute.internal>
2019-04-16 19:09:24 +0000

    xss fixed

--- memos.js
+++ memos.js
@@ -19,7 +19,7 @@ var db = require("./database.js");
 
 function addMemos(req,res,next)
 {
-   var memo = req.body.memo;
+   var memo = req.body.memo.replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;");
 
    var q = "INSERT INTO Memos(memo) VALUES ( '" + memo + "' )";
    db.query(q,function(e1,d1) { addMemos1(req,res,next,e1,d1); });
@@ -55,7 +55,7 @@ function displayMemos(req,res,next)
 function displayMemos1(req,res,next,err,data)
 {
     if (err) next(err);
-    
+ 
     var doc = { memosList: data.rows, userId : req.session.userId };
     
     return res.render("memos",doc);
